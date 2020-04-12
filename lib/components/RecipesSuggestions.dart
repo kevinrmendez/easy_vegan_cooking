3b7b56f0b822e369abc6864795e84799e22cbd50
Kevin Mendez <kevinrmendez@gmail.com>
2020-04-12 16:53:20 +0300

    screen  splash added

--- lib/components/RecipesSuggestions.dart
+++ lib/components/RecipesSuggestions.dart
@@ -71,6 +71,7 @@ class _RecipesSuggestionsState extends State<RecipesSuggestions> {
         steps: data["steps"],
         labels: data["labels"],
         nutrition: data["nutrition"],
+        attribution: data["attribution"],
         isFavorite: false);
   }
 
@@ -91,16 +92,19 @@ class _RecipesSuggestionsState extends State<RecipesSuggestions> {
             builder: (context, snap) {
               switch (snap.connectionState) {
                 case ConnectionState.waiting:
-                  return Center(
-                      child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                          height: 50,
-                          width: 50,
-                          child: CircularProgressIndicator()),
-                    ],
-                  ));
+                  return Container(
+                    height: 300,
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                            height: 50,
+                            width: 50,
+                            child: CircularProgressIndicator()),
+                      ],
+                    )),
+                  );
                 default:
                   // print("SNAPSHOT: ${snap.data.snapshot.value}");
                   if (snap.hasData &&
@@ -141,73 +145,89 @@ class _RecipesSuggestionsState extends State<RecipesSuggestions> {
                                   _recipeBuilder(filteredRecipes[index]);
                               // return Text(recipe.title);
                               return GestureDetector(
-                                child: Stack(
-                                    alignment: AlignmentDirectional.topEnd,
-                                    fit: StackFit.loose,
-                                    children: <Widget>[
-                                      Container(
-                                        width: 300,
-                                        child: Card(
-                                          child: CachedNetworkImage(
-                                            imageBuilder:
-                                                (context, imageProvider) =>
-                                                    Container(
-                                              decoration: BoxDecoration(
-                                                  image: DecorationImage(
-                                                      image: imageProvider,
-                                                      fit: BoxFit.cover)),
-                                            ),
-                                            imageUrl: recipe.image,
-                                            placeholder: (context, url) =>
-                                                Container(
-                                              color: GreyColor,
-                                              constraints: BoxConstraints(
-                                                  maxHeight: 30, maxWidth: 30),
-                                              child: Column(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment.center,
-                                                children: <Widget>[
-                                                  SizedBox(
-                                                      height: 40,
-                                                      width: 40,
-                                                      child:
-                                                          new CircularProgressIndicator()),
-                                                ],
+                                child: Card(
+                                  child: Stack(
+                                      alignment: AlignmentDirectional.topEnd,
+                                      fit: StackFit.loose,
+                                      children: <Widget>[
+                                        Container(
+                                          width: 300,
+                                          child: Card(
+                                            child: CachedNetworkImage(
+                                              // height: 300,
+                                              imageBuilder:
+                                                  (context, imageProvider) =>
+                                                      Container(
+                                                decoration: BoxDecoration(
+                                                    image: DecorationImage(
+                                                        image: imageProvider,
+                                                        fit: BoxFit.cover)),
                                               ),
+                                              imageUrl: recipe.image,
+                                              placeholder: (context, url) =>
+                                                  Container(
+                                                height: 300,
+                                                color: GreyColor,
+                                                child: Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  children: <Widget>[
+                                                    SizedBox(
+                                                        height: 40,
+                                                        width: 40,
+                                                        child:
+                                                            new CircularProgressIndicator()),
+                                                  ],
+                                                ),
+                                              ),
+                                              errorWidget: (context, url,
+                                                      error) =>
+                                                  Container(
+                                                      height: 300,
+                                                      color: GreyColor,
+                                                      constraints:
+                                                          BoxConstraints(
+                                                              maxHeight: 30,
+                                                              maxWidth: 30),
+                                                      child: Icon(Icons.error)),
                                             ),
-                                            errorWidget: (context, url,
-                                                    error) =>
-                                                Container(
-                                                    color: GreyColor,
-                                                    constraints: BoxConstraints(
-                                                        maxHeight: 30,
-                                                        maxWidth: 30),
-                                                    child: Icon(Icons.error)),
                                           ),
                                         ),
-                                      ),
-                                      Container(
-                                        padding: EdgeInsets.all(4),
-                                        color: Color.fromRGBO(0, 0, 0, 0.3),
-                                        margin: EdgeInsets.all(10),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.end,
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: <Widget>[
-                                            Text(
-                                              recipe.title,
-                                              style: TextStyle(
-                                                  color: Colors.white),
+                                        Positioned(
+                                          bottom: 0,
+                                          width: 300,
+                                          child: Container(
+                                            color: Colors.white,
+                                            padding:
+                                                EdgeInsets.fromLTRB(0, 7, 6, 7),
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.end,
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: <Widget>[
+                                                Text(
+                                                  recipe.title,
+                                                  style: TextStyle(
+                                                      // color: Colors.white
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 16,
+                                                      color: Colors.black),
+                                                ),
+                                                Text(" ${recipe.time} min",
+                                                    textAlign: TextAlign.right,
+                                                    style: TextStyle(
+                                                        // color: Colors.white
+                                                        fontSize: 16,
+                                                        color: Colors.black))
+                                              ],
                                             ),
-                                            Text(" ${recipe.time} min",
-                                                textAlign: TextAlign.right,
-                                                style: TextStyle(
-                                                    color: Colors.white))
-                                          ],
-                                        ),
-                                      )
-                                    ]),
+                                          ),
+                                        )
+                                      ]),
+                                ),
                                 onTap: () {
                                   _showAd();
                                   Navigator.push(
